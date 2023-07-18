b8e6845e141c636502a908f8ddb381ddd12a73e0
Kirsten Carr <kirstencarrlc25@gmail.com>
2023-07-18 06:52:40 -0700

    finished math quiz

--- csc221/Sheet2/mathquiz.py
+++ csc221/Sheet2/mathquiz.py
@@ -1,2 +1,15 @@
-print(int("What is 2 times 5?"))
-answer = int(input(question))
+from random import randint
+score = 0
+for x in range (3): 
+    num1 = randint(1, 10)
+    num2 = randint(1, 10)
+    question = "What is " + str(num1) + " times " + str(num2) + "? "
+
+    answer = int(input(question))
+    if answer == num1 * num2:
+        print("You're right")
+        score = score + 1
+    if answer != num1 * num2:
+        print('Try again')
+
+print("You got " + str(score) + " questions right.")
---
+++ csc221/sheet 5/Robot_game.py
@@ -0,0 +1,14 @@
+from gasp import *
+
+begin_graphics()
+finished = False
+def place_player_(x):
+    print("I'm right here!")
+def move_player():
+    print("I'm moving to...")
+    update_when('key_pressed')
+
+while not finished:
+    move_player()
+
+    end_graphics()
