cfb17aea7f7dc290f85804c0086c77763b871466
Kirsten Carr <kirstencarrlc25@gmail.com>
2023-07-19 08:16:55 -0700

    purple robot working

--- csc221/Sheet 3/prettypictures.py
+++ csc221/Sheet 3/prettypictures.py
@@ -1,5 +1,12 @@
 from gasp import *
 begin_graphics()
-Line((200,100), (100,300))
+for r in 100, 110, 120, 130, 140:
+    Circle((300, 200), 100)#head
+    Arc((300, 200), 50, 225, 315)#smile
+    Circle((260, 230), 20)#left eye
+    Circle((338, 230), 20)#right eye
+    Circle((340, 232), 5.5)#left pulip
+    Circle((262, 232), 5.5)#right pupil
+
 update_when('key_pressed')
 end_graphics 
--- csc221/sheet 5/Robot_game.py
+++ csc221/sheet 5/Robot_game.py
@@ -1,29 +1,34 @@
 from gasp import *
+from random import randint
+
+
+def place_player():
+    global player_x, player_y, player_shape
+
+    print("I'm right here!")
+    player_x  = randint(0, 63)
+    player_y = randint(0, 47)
+    player_shape = Circle((10 * player_x + 5, 10 * player_y +5 ), 5, filled=True, color=color.PURPLE)
+
+
+def move_player():
+    global finished, player_shape, player_x, player_y
+
+    print("I'm moving to...")
+    key = update_when('key_pressed')
+    if key == 'q':
+        finished = True
+    if key == 'd':
+        player_x += 1
+
+    move_to(player_shape, (10 * player_x + 5, 10 * player_y + 5))
+
 
 begin_graphics()
-c = Circle((320, 200), 5)
-move_to(c, (350,220))
-from random import randint
-num1=(0,63)
-num2=(0, 47)
-player_x= num1
-player_y= num2
-Circle((10 * player_x + 5, 10 * player_y +5), 5, filled= True)
-finished = False 
-def place_player():
-print ("I'm right here!")
-move_player():
-print("I'm moving to...")
-update_when ("Key_pressed")
-
 finished = False
-def place_player_(x):
-    print("I'm right here!")
-def move_player():
-    print("I'm moving...")
-    update_when('key_pressed')
+place_player()
 
 while not finished:
     move_player()
 
-    end_graphics()
+end_graphics()
